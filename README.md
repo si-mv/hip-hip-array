b2e4b58e8fdd306f390433fb97405fbc669ab7f1
si-mv <simon.haines@multiverse.io>
2022-07-18 17:04:56 +0100

    adds readme with excellent pun

---
+++ README.md
@@ -0,0 +1,9 @@
+# Array Potter
+
+Fork it, clone it, bop it, twist it!
+
+Once you have it on your machine, run `npm install` and `npm run test`.
+
+You should find lots of failing tests. Now you can go c`[]`zy solving all of the challenges!
+
+Good luck :D
--- challenges/arraySameUnordered.js
+++ challenges/arraySameUnordered.js
@@ -2,7 +2,7 @@
  * Return true if the arrays contain the same elements in any order, else false
  * e.g. ([1, 2], [1, 2]) => true
  * e.g. ([1, 2], [2, 1]) => true
- * e.g. ([1,2], [2,3]) => false
+ * e.g. ([1, 2], [2, 3]) => false
  * @param {(number|string)[]} arr1 
  * @param {(number|string)[]} arr2
  * @returns {boolean}
